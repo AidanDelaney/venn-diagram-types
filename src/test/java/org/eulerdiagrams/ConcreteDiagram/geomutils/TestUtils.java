025877ebc59cb8a1af9a0589f76ed163cd1a6233
Aidan Delaney <aidan@ontologyengineering.org>
2015-09-03 11:00:17 +0100

    Test that the directionlessFind method works

--- src/test/java/org/eulerdiagrams/ConcreteDiagram/geomutils/TestUtils.java
+++ src/test/java/org/eulerdiagrams/ConcreteDiagram/geomutils/TestUtils.java
@@ -8,6 +8,7 @@ import org.junit.Test;
 
 import java.util.Collection;
 import java.util.Optional;
+import java.util.Vector;
 
 import static org.junit.Assert.*;
 import static org.hamcrest.Matchers.*;
@@ -88,8 +89,39 @@ public class TestUtils {
         Collection<Point2D> ixs = oixs.get();
         ixs.remove(ipoint);
 
-        CircleArc2D a  = Utils.next(curve1, arc, curve2, new BoundaryPolyCurve2D<CircleArc2D>());
-        assertThat(a, is(not(arc)));
-        assertThat(curve1.contains(a), is(true));
+        Pair<Optional<CircleArc2D>, Optional<CircleArc2D>> a  = Utils.next(curve1, curve2, new Vector<CircleArc2D>(), ipoint);
+        assertThat(a.car, is(not(Optional.empty())));
+        assertThat(a.cdr, is(not(Optional.empty())));
+
+    }
+
+    @Test
+    public void testDirectionlessFind () {
+        Circle2D c1 = new Circle2D(0, 0, 10);
+        Circle2D c2 = new Circle2D(5, 0, 10);
+
+        CircleArc2D ca1 = new CircleArc2D(c1, 0, Math.PI * 2); // a full circle
+        CircleArc2D ca2 = new CircleArc2D(c2, 0, Math.PI * 2); // a full circle
+
+        BoundaryPolyCurve2D<CircleArc2D> curve1 = new BoundaryPolyCurve2D<>(), curve2 = new BoundaryPolyCurve2D<>();
+        curve1.add(ca1);
+        curve2.add(ca2);
+
+        assertThat(curve1.contains(ca1), is(true));
+
+        Optional<Collection<Point2D>> oixs = ca1.intersections(ca2);
+
+        for(Point2D p : oixs.get()) {
+            curve1 = Utils.split(curve1, p);
+            curve2 = Utils.split(curve2, p);
+        }
+        assertThat(curve1.curves().size(), is(3));
+        assertThat(curve2.curves().size(), is(3));
+
+        Point2D ipoint = oixs.get().toArray(new Point2D[0])[0]; // first intersection point
+
+        Optional<CircleArc2D> oc = Utils.directionlessFind(curve1, new Vector<CircleArc2D>(), ipoint);
+
+        assertThat(oc.isPresent(), is(true));
     }
 }
