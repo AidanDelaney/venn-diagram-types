0af3454518add86de01a6709e69c6b3158661439
Aidan Delaney <aidan@ontologyengineering.org>
2015-12-10 19:11:37 +0000

    Revert "Removed files that are no-longer used."
    This reverts commit acb216745914123f8e857e6779f8b283c1c5c556.

---
+++ src/test/java/org/eulerdiagrams/AbstractDiagram/TestVennSetIterator.java
@@ -0,0 +1,41 @@
+package org.eulerdiagrams.AbstractDiagram;
+
+import static org.junit.Assert.*;
+
+import org.eulerdiagrams.AbstractDiagram.AbstractContour;
+import org.eulerdiagrams.AbstractDiagram.VennSetIterator;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static org.hamcrest.Matchers.*;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class TestVennSetIterator {
+    private final static Logger logger = LoggerFactory.getLogger(TestVennSetIterator.class);
+
+    @Test
+    public void testBasicIterator() {
+        VennSetIterator it = new VennSetIterator(Arrays.asList());
+
+        AbstractZone z = it.next();
+        assertThat(z, is(equalTo(new AbstractZone(new HashSet<>(), new HashSet<>()))));
+        assertThat(it.hasNext(), is(false));
+    }
+
+    @Test
+    public void testIterator() {
+        AbstractContour a = new AbstractContour("A");
+        AbstractContour b = new AbstractContour("B");
+        AbstractContour c = new AbstractContour("C");
+
+        int i=0;
+        for(AbstractZone z: new VennSetIterator(Arrays.asList(a, b, c))) { // testing for-each here
+            logger.info(z.toString());
+            i++;
+        }
+        assertThat(i, is((int) Math.pow(2,3)));
+    }
+}
